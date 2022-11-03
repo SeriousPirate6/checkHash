e6d03a600d78f3e906f8a6c3387394ec4758251e
SeriousPirate6 <whatyoudontexpect3@gmail.com>
2022-11-03 23:47:25 +0100

    Loop for nested search and MD5 check controls.

--- checkHash.bat
+++ checkHash.bat
@@ -3,13 +3,36 @@
 @REM -
 @REM Constants
 @REM -
+set sub=Output
 set loc=location.txt
+set hash=hash.txt
 
 @REM -
 @REM Checking param files existance
 @REM -
-if not exist %loc% type NUL > %loc%
+if not exist %sub% mkdir %sub%
+if not exist %sub%\%loc% type NUL > %sub%\%loc%
 
-set /p "file=Enter the file you want to copy and check with hash MD5: "
+type NUL > %sub%\%hash%
+
+set /p "source=Directory to copy and get every file checked with hash MD5: "
 
 set /p pro=< %loc%
+
+@REM -
+@REM Getting the MD5 hash of all files in the selected directory
+@REM -
+For /R .\%sub% %%G IN (*.*) do (
+    @REM -
+    @REM filtering all the lines of the output that not contains ":"
+    @REM -
+    certutil -hashfile "%%G" MD5 | findstr /V ":">> %sub%\%hash%
+    @REM -
+    @REM error 0 represents the ordinary execution of the command
+    @REM -
+    if errorlevel 0 (
+        echo Checksum completed!
+	) else echo Cannot perform the checksum on this file!
+)
+
+pause>nul
